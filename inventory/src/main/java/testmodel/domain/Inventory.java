4a33779bc3ad1e0d1b763c358f226ecdc6de9264
uengineYSW <112850933+uengineYSW@users.noreply.github.com>
2023-10-31 13:47:25 +0900

    commit

--- inventory/src/main/java/testmodel/domain/Inventory.java
+++ inventory/src/main/java/testmodel/domain/Inventory.java
@@ -1,46 +1,61 @@
-package testmodel.domaiccasn;
+package testmodel.domain;
 
-import java.util.Date;
-import java.util.List;
-import javax.persistence.*;
-import lombok.Data;
 import testmodel.InventoryApplication;
+import javax.persistence.*;aa
+import java.util.List;
+import lombok.Data;ew
+import java.util.Date;dsada
+
 
 @Entity
-@Table(name = "Inventory_table")
+@Table(name="Inventory_table")
 @Data
-//<<< DDD / Aggregate Root
-public class Inventory {
 
+//<<< DDD / Aggregate Root
+public class Inventory  {
+
+
+    
     @Id
     //@GeneratedValue(strategy=GenerationType.AUTO)
-
+    
+    
+    
+    
+    
     private Long productId;
-
+    
+    
+    
+    
+    
     private Integer stock;
 
     @PostPersist
-    public void onPostPersist() {}
+    public void onPostPersist(){
+    }
 
-    public static InventoryRepository repository() {
-        InventoryRepository inventoryRepository = InventoryApplication.applicationContext.getBean(
-            InventoryRepository.class
-        );
+    public static InventoryRepository repository(){
+        InventoryRepository inventoryRepository = InventoryApplication.applicationContext.getBean(InventoryRepository.class);
         return inventoryRepository;
     }
 
-    //<<< Clean Arch / Port Method
-    public void decreaseStock(DecreaseStockCommand decreaseStockCommand) {
-        //implement business logic here:
 
+
+//<<< Clean Arch / Port Method
+    public void decreaseStock(DecreaseStockCommand decreaseStockCommand){
+        
+        //implement business logic here:
+        
         InventoryUpdated inventoryUpdated = new InventoryUpdated(this);
         inventoryUpdated.publishAfterCommit();
+
     }
+//>>> Clean Arch / Port Method
 
-    //>>> Clean Arch / Port Method
-
-    //<<< Clean Arch / Port Method
-    public static void updateInventory(OrderPlaced orderPlaced) {
+//<<< Clean Arch / Port Method
+    public static void updateInventory(OrderPlaced orderPlaced){
+        
         //implement business logic here:
 
         /** Example 1:  new item 
@@ -64,8 +79,10 @@ public class Inventory {
          });
         */
 
+        
     }
-    //>>> Clean Arch / Port Method
+//>>> Clean Arch / Port Method
+
 
 }
 //>>> DDD / Aggregate Root
