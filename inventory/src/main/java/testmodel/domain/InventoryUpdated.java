3cab6cab706aea51d499e74f88cb001f44cdca88
uengineYSW <112850933+uengineYSW@users.noreply.github.com>
2023-11-01 10:03:26 +0900

    commit

--- inventory/src/main/java/testmodel/domain/InventoryUpdated.java
+++ inventory/src/main/java/testmodel/domain/InventoryUpdated.java
@@ -1,23 +1,4 @@
-package testmodel.domain;
-
-import java.util.*;
-import lombok.*;
-import testmodel.domain.*;
-import testmodel.infra.AbstractEvent;
-
-@Data
-@ToString
-@NoArgsConstructor
-public class InventoryUpdated extends AbstractEvent {
-
-    private Long productId;
-    private Integer stock;
-
-    public InventoryUpdated() {
-        super();
-    }
-
-    public InventoryUpdated(Inventory aggregate) {
+    @NoArgConstructor
+    public InventoryUpdated(Inventory aggregate){
         super(aggregate);
     }
-}
