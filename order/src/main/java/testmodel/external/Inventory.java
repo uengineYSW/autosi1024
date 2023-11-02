c577bd8cdc109af9e26f43eff116be4564b8eecd
uengineYSW <112850933+uengineYSW@users.noreply.github.com>
2023-11-02 15:36:34 +0900

    commit

--- order/src/main/java/testmodel/external/Inventory.java
+++ order/src/main/java/testmodel/external/Inventory.java
@@ -1,11 +1,34 @@
-package testmodel.external;
+package testmodel.domain;
 
 import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
 import lombok.Data;
+import testmodel.InventoryApplication;
 
+@Entity
+@Table(name = "Inventory_table")
 @Data
 public class Inventory {
 
+    @Id
     private Long productId;
+
     private Integer stock;
+
+    @PostPersist
+    public void onPostPersist() {}
+
+    public static InventoryRepository repository() {
+        InventoryRepository inventoryRepository = InventoryApplication.applicationContext.getBean(
+            InventoryRepository.class
+        );
+        return inventoryRepository;
+    }
+
+    public void decreaseStock(DecreaseStockCommand decreaseStockCommand) {
+        this.stock -= decreaseStockCommand.getQty();
+        InventoryUpdated inventoryUpdated = new InventoryUpdated(this);
+        inventoryUpdated.publishAfterCommit();
+    }
 }
--- order/src/main/java/testmodel/infra/PolicyHandler.java
+++ order/src/main/java/testmodel/infra/PolicyHandler.java
@@ -1,24 +1,43 @@
 package testmodel.infra;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import javax.naming.NameParser;
-import javax.naming.NameParser;
-import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
-import testmodel.config.kafka.KafkaProcessor;
+import testmodel.domain.*;
 import testmodel.domain.*;
 
 @Service
-@Transactional
 public class PolicyHandler {
 
     @Autowired
-    OrderRepository orderRepository;
+    InventoryRepository inventoryRepository;
 
     @StreamListener(KafkaProcessor.INPUT)
     public void whatever(@Payload String eventString) {}
+
+    @StreamListener(
+        value = KafkaProcessor.INPUT,
+        condition = "headers['type']=='OrderPlaced'"
+    )
+    public void wheneverOrderPlaced_UpdateInventory(
+        @Payload OrderPlaced orderPlaced
+    ) {
+        OrderPlaced event = orderPlaced;
+        System.out.println(
+            "\n\n##### listener UpdateInventory : " + orderPlaced + "\n\n"
+        );
+
+        // Sample Logic //
+        Inventory inventory = inventoryRepository
+            .findById(event.getProductId())
+            .orElse(null);
+        if (inventory != null) {
+            DecreaseStockCommand decreaseStockCommand = new DecreaseStockCommand();
+            decreaseStockCommand.setProductId(event.getProductId());
+            decreaseStockCommand.setQty(event.getQty());
+            inventory.decreaseStock(decreaseStockCommand);
+            inventoryRepository.save(inventory);
+        }
+    }
 }
